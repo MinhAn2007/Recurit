2d188ead0fc5464c134246abe40e5ca7627dee67
MinhAn <voongocminhan20072002@gmail.com>
2023-11-27 16:32:31 +0700

    Mapping

---
+++ src/main/java/vn/edu/iuh/fit/www_lab05/models/CandidateSkill.java
@@ -0,0 +1,30 @@
+package vn.edu.iuh.fit.www_lab05.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import vn.edu.iuh.fit.www_lab05.enums.SkillLevel;
+import vn.edu.iuh.fit.www_lab05.ids.CandidateSkillID;
+@Entity
+@Table(name = "candidate_skill")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@IdClass(CandidateSkillID.class)
+public class CandidateSkill {
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "skill_id")
+    private Skill skill;
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "can_id")
+    private Candidate candidate;
+
+    @Column(name = "skill_level", nullable = false)
+    private SkillLevel skillLevel;
+    @Column(name = "more_infos", length = 1000)
+    private String moreInfo;
+}
